763dec3df919e820026cb5430d7600cb68729df5
isaacsaavedra02 <isaacsaavedraa020104@gmail.com>
2024-10-28 01:50:43 +0000

    xd

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,12 +1,35 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * times_table - ESTA VERGA ME VA LLEVANDO 2 DIAS LPM
  */
-int main(void)
+void times_table(void)
 {
-    times_table();
-    return (0);
+	int fila, columna, resultado;
+
+	for (fila = 0; fila <= 9; fila++)
+	{
+	for (columna = 0; columna <= 9; columna++)
+	{
+	resultado = fila * columna;
+
+	if (resultado < 10)
+	{
+	if (columna > 0)
+	{
+	_putchar(',');
+	 _putchar(' ');
+	 _putchar(' ');
+	}
+	_putchar(resultado + '0');
+	}
+	else
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar((resultado / 10) + '0');
+	 _putchar((resultado % 10) + '0');
+	}
+	}
+	_putchar('\n');
+	}
 }
