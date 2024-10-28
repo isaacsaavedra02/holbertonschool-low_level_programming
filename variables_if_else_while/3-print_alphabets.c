b03bb0c9fbe4233084ea5be9f3fc13899ae35209
isaacsaavedra02 <isaacsaavedraa020104@gmail.com>
2024-10-28 00:28:52 +0000

    3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char xd;
+
+	for (xd = 'a'; xd <= 'z'; xd++)
+	{
+		putchar(az);
+	}
+	putchar('\n');
+	return (0);
+}
