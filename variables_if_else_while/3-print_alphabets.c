b7c7855a1a9126b0cb286d90c5dfc3a549efea4e
isaacsaavedra02 <isaacsaavedraa020104@gmail.com>
2024-10-28 00:35:18 +0000

    3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+
 /**
- * main - alphabet in lowercase
+ * main - Print the alphabet in lowercase
  * Return: 0
  */
 int main(void)
 {
-	char xd;
+    char xd;
 
-	for (xd = 'a'; xd <= 'z'; xd++)
-	{
-		putchar(xd);
-	}
-	putchar('\n');
-	return (0);
+    for (xd = 'a'; xd <= 'z'; xd++)
+    {
+        putchar(xd);
+    }
+    putchar('\n');
+    return (0);
 }
