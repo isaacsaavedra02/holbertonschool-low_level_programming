532818a260cf62854b0d706323a1c59f30adb81d
isaacsaavedra02 <isaacsaavedra020104@gmail.com>
2024-11-14 13:18:02 +0000

    0i

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_isupper- función que indica si un carácter o cadena de caracteres es una letra mayúscula
+ *@c: variable que da mayusculas
+ *Return: 1 si es mayuscula 0 si no
+ */
+
+int _isupper(int c)
+{
+
+if (c >= 'A' && c <= 'Z')
+
+	return (1);
+	else
+	return (0);
+}
