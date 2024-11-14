ffec394f83f4812a8371c20d6a6351163b084d6f
isaacsaavedra02 <isaacsaavedra020104@gmail.com>
2024-11-14 13:37:45 +0000

    0i

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,27 @@
 #include "main.h"
 
 /**
- *_isupper- función que indica si un carácter o cadena de caracteres es una letra mayúscula
- *@c: variable que da mayusculas
- *Return: 1 si es mayuscula 0 si no
+ *_isupper - función que indica si un carácter es una letra mayúscula
+ *@c: variable que da mayúsculas
+ *Return: 1 si es mayúscula, 0 si no
+ */
+int _isupper(int c) {
+	if (c >= 'A' && c <= 'Z')
+		return 1;
+	else
+		return 0;
+}
+
+/**
+ * main - función principal para probar _isuper
+ * Return: 0
  */
 int main() {
-int _isupper(int c)
-{
+	char c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
 
-if (c >= 'A' && c <= 'Z')
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
 
-	return (1);
-	else
-	return (0);
-}
-return 0;
+	return 0;
 }
