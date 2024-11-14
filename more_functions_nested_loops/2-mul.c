31e2917f04806da0f161f48694685a906e03a51b
isaacsaavedra02 <isaacsaavedra020104@gmail.com>
2024-11-14 16:31:47 +0000

    2mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplica dos enteros
+ * @a: primer numero a multiplicar
+ * @b: segundo numero a multiplicar
+ * Return: debe dar el resultado de a y b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
