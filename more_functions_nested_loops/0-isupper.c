8e0866d655533f435d5cadf6ead7230446de1d87
isaacsaavedra02 <isaacsaavedra020104@gmail.com>
2024-11-14 13:23:20 +0000

    0i

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -5,7 +5,7 @@
  *@c: variable que da mayusculas
  *Return: 1 si es mayuscula 0 si no
  */
-
+int main() {
 int _isupper(int c)
 {
 
@@ -14,4 +14,6 @@ if (c >= 'A' && c <= 'Z')
 	return (1);
 	else
 	return (0);
+}
+return 0;
 }
